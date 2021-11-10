e88e61980151f15b34f2df954d74e1789e838f2f
Andrew Ngei <kimngei@gmail.com>
2021-11-10 19:47:20 +0300

    latest

--- database/sqlite/migrations/2_create_table_providers_up.sql
+++ database/sqlite/migrations/2_create_table_providers_up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE providers(
     provider_id VARCHAR(255) PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL UNIQUE,
-    profile_id VARCHAR(255) NOT NULL UNIQUE,
     bio TEXT,
     profession VARCHAR(255),
     ratings_average INT(11) DEFAULT 0,
