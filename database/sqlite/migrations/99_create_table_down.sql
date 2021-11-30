fb3cd7499a66881336464f2233536377b0055114
Andrew Ngei <kimngei@gmail.com>
2021-11-30 12:08:37 +0300

    reset db

---
+++ database/sqlite/migrations/99_create_table_down.sql
@@ -0,0 +1,1 @@
+DROP TABLE `users`, `categories`, `locations`, `bids`, `bookings`, `booking_photos`, `clients`, `industries`, `migrations`, `photos`, `portfolios`, `portfolio_photos`, `providers`, `provider_professions`, `rates`, `reviews`, `services`, `transactions`, `user_locations`;
