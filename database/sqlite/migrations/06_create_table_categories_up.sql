13dfba480496d5844c34426a10ecce254b2cbf4a
Andrew Ngei <kimngei@gmail.com>
2021-12-03 08:08:36 +0300

    correct foreign key types

--- database/sqlite/migrations/06_create_table_categories_up.sql
+++ database/sqlite/migrations/06_create_table_categories_up.sql
@@ -5,7 +5,7 @@ CREATE TABLE categories(
     description TEXT,
     level INT(20) DEFAULT 0,
     icon_url VARCHAR(255) NOT NULL,
-    industry_id INT(20),
+    industry_id INTEGER,
     FOREIGN KEY (parent_id) references categories(id),
     FOREIGN KEY (industry_id) references industries(id)
 );
